9667f9a756ea56e2ef4d0475b132bef91343a766
Ruhul amin <ruhul.cse7862@gmail.com>
2018-01-24 09:15:54 +0600

    Create Life, the Universe, and Everything.cpp

---
+++ Beginer/Life, the Universe, and Everything.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  cout<<n<<endl;
+  while(true)
+  {
+     int a;
+     cin>>a;
+     if(a==42)
+     break;
+     else
+     cout<<a<<endl;
+  }
+  return 0;
+}
