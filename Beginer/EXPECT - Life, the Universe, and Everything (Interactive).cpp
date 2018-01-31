50e1d95658c40b1e5b925708b2b08e5e171f0114
Ruhul amin <ruhul.cse7862@gmail.com>
2018-01-31 09:00:26 +0600

    ID 2730

---
+++ Beginer/EXPECT - Life, the Universe, and Everything (Interactive).cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   while(true)
+   {
+       cin>>n;
+       cout<<n<<endl;
+       fflush(stdout);
+       if(n==42)
+        break;
+   }
+   return 0;
+}
